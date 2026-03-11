e094a92b9ce60aab011ca1d92e2883080553d755
Vladimir Aleshkov <vovten@mail.ru>
2026-03-11 10:18:18 +0600

    ref:remove empty lines

--- event-flow-spring/src/main/java/com/github/vovten/eventflow/autoconfig/config/PublisherConfiguration.java
+++ event-flow-spring/src/main/java/com/github/vovten/eventflow/autoconfig/config/PublisherConfiguration.java
@@ -39,42 +39,30 @@ public class PublisherConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public EventPublisher eventPublisher(List<EventChannel> eventChannels) {
-
         EventFlowProperties.PublisherConfig publisherConfig = properties.getPublisher();
-
         log.info("Configuring EventPublisher with {} channels: {}",
             eventChannels.size(),
             eventChannels.stream()
                 .map(EventChannel::name)
-                .collect(joining(", ")));
-
-        log.debug("Publisher configuration: transactional={}, retry={}, silent={}",
-                publisherConfig.isTransactional(),
-                publisherConfig.getRetry().isEnabled(),
-                publisherConfig.isSilent());
-
+                .collect(joining(", "))
+        );
         EventPublisherBuilder builder = EventPublisherBuilder.channels(eventChannels);
-
         // Apply retry if enabled
         var retry = publisherConfig.getRetry();
         if (retry.isEnabled()) {
             builder.retryable(retry.getMaxAttempts(), retry.getInitialDelay(), retry.getMultiplier());
         }
-
         EventPublisher publisher = builder.buildAndLog();
-
         // Wrap with transactional decorator if enabled
         if (publisherConfig.isTransactional()) {
             log.info("Wrapping publisher with TransactionalEventPublisher");
             publisher = new TransactionalEventPublisher(publisher);
         }
-
         // Apply silent mode as the outermost decorator (suppresses all exceptions)
         if (publisherConfig.isSilent()) {
             log.info("Wrapping publisher with SilentEventPublisher");
             publisher = new SilentEventPublisher(publisher);
         }
-
         return publisher;
     }
 }
